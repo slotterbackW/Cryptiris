d1d155428e6481faa41d391123609904f91e38ca
Charlie Peters <chpeters13@gmail.com>
2017-11-10 18:31:43 -0500

    fixed a little bit about users api

--- lib/cryptiris_web/controllers/user_controller.ex
+++ lib/cryptiris_web/controllers/user_controller.ex
@@ -10,9 +10,13 @@ defmodule CryptirisWeb.UserController do
   def create(conn, %{"user" => user_params}) do
     case Accounts.create_user(user_params) do
       {:ok, user} ->
-        render(conn, "show.json", user: user)
+      	conn
+      	|> put_status(:created)
+      	|> render("show.json", user: user)
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "show.json", changeset: changeset)
+      	conn
+      	|> put_status(:unprocessable_entity)
+        |> render(CryptirisWeb.ChangesetView, "error.json", changeset: changeset)
     end
   end
 
@@ -20,9 +24,13 @@ defmodule CryptirisWeb.UserController do
     user = Accounts.get_user!(id)
     case Accounts.update_user(user, user_params) do
       {:ok, user} ->
-        render(conn, "show.json", user: user)
+        conn
+      	|> put_status(:created)
+      	|> render("show.json", user: user)
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "show.json", changeset: changeset)
+        conn
+      	|> put_status(:unprocessable_entity)
+        |> render(CryptirisWeb.ChangesetView, "error.json", changeset: changeset)
     end
   end
 
--- lib/cryptiris_web/views/user_view.ex
+++ lib/cryptiris_web/views/user_view.ex
@@ -1,13 +1,15 @@
 defmodule CryptirisWeb.UserView do
   use CryptirisWeb, :view
-  alias CryptirisWeb.UserView
 
   def render("show.json", %{user: user}) do
     %{data: render_one(user, ExchangeView, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
-    user
+    %{
+      id: user.id,
+      email: user.email
+    }
   end
 
   def render("success.json", %{message: message}) do
