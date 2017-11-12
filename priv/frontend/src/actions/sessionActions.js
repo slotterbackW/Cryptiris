617a8c4da9d61a9b505ae144ea6b0a2c97fa9151
Charlie Peters <chpeters13@gmail.com>
2017-11-11 20:34:44 -0500

    added base actions and reducer shells

---
+++ priv/frontend/src/actions/sessionActions.js
@@ -0,0 +1,44 @@
+import api from '../utils/api'
+
+export const LOGIN_ATTEMPTING = 'LOGIN_ATTEMPTING'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+export function loginSuccess(data) {
+	return {
+		type: LOGIN_SUCCESS,
+		data
+	}
+}
+
+export const LOGOUT_ATTEMPTING = 'LOGOUT_ATTEMPTING'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+
+export function login(username, password) {
+	return (dispatch) => {
+		dispatch({
+			type: LOGIN_ATTEMPTING
+		})
+		return api.login(email, password).then((data) = > {
+			dispatch(loginSuccess(data))
+		}).catch(error) {
+			dispatch({
+				type: LOGIN_FAILURE
+			})
+			throw error
+		}
+	}
+}
+
+export function logout() {
+	return (dispatch) => {
+		dispatch({
+			type: LOGOUT_ATTEMPTING
+		})
+	}
+	return api.logout().then(() => {
+		dispatch({
+			type: LOGOUT_SUCCESS
+		})
+	})
+}
