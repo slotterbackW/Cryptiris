3e1282e04ef43200dcc5167e67ab1b52900253b7
Will Slotterback <will2337@gmail.com>
2017-11-13 12:51:08 -0500

    Add login component

--- priv/frontend/src/components/Login/index.js
+++ priv/frontend/src/components/Login/index.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Form, Field } from 'simple-react-form'
+import { PasswordField, EmailField } from '../FormComponents/TextFields'
+import * as actions from '../../actions/sessionActions'
 
-const Login = () => (
-    <div className="container">
-        <h1>Login</h1>
-    </div>
-)
+class Login extends React.Component {
+    onSubmit = ({email, password}) => {
+        this.props.dispatch(actions.login(email, password))
+    }
 
-export default Login
+    state = {}
+
+    render() {
+        if (this.props.loading) {
+            return (
+                <div className="container">
+                    <h1>Login</h1>
+                    <p> loading ... </p>
+                </div>
+            )
+        } else if (this.props.error) {
+            return (
+                <div className="container">
+                    <h1>Login</h1>
+                    <p>Error: {this.props.error.toString()} </p>
+                    <Form ref='form' state={this.props.initialDoc} onSubmit={this.onSubmit}>
+						<Field fieldName='email' label='Email' type={EmailField} />
+						<Field fieldName='password' label='Password' type={PasswordField} />
+                    </Form>
+                </div>
+            )
+        } else {
+            return (
+                <div className="container">
+                    <h1>Login</h1>
+                    <Form ref='form' state={this.props.initialDoc} onSubmit={this.onSubmit}>
+                        <Field fieldName='email' label='Email' type={EmailField} />
+                        <Field fieldName='password' label='Password' type={PasswordField} />
+                    </Form>
+                    <button onClick={() => this.refs.form.submit()}>Login</button>
+                </div>
+            )
+        }
+    }
+}
+
+export default connect(
+    (state) => state.user
+)(Login)
