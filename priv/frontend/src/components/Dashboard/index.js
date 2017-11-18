923fc3a24b1d529fe296d6f1a1aca37fac873043
Will Slotterback <will2337@gmail.com>
2017-11-17 21:35:04 -0800

    Add some temporary placeholders for the dashboard page

--- priv/frontend/src/components/Dashboard/index.js
+++ priv/frontend/src/components/Dashboard/index.js
@@ -3,6 +3,8 @@ import { LineChart, Line, YAxis, XAxis } from 'recharts'
 
 import MainNav from '../MainNav'
 
+import './dashboard.css'
+
 const btc = [
     {date: "10/24/2017", value: 5509.50},
     {date: "10/25/2017", value: 5720.00},
@@ -20,11 +22,67 @@ export default class Dashboard extends React.Component {
                 <MainNav />
                 <div className="container">
                     <h1>Dashboard</h1>
-                    <LineChart width={400} height={200} data={btc}>
-                        <Line type="monotone" dataKey="value" stroke="#f00" />
-                        <XAxis dataKey="date"/>
-                        <YAxis allowDecimals={false} type="number" domain={['dataMin - 200', 'dataMax + 200']} />
-                    </LineChart>
+                    <div>
+                        <div className="form-group">
+                            <label>Cryptocurrency</label>
+                            <select>
+                                <option value="Bitcoin">Bitcoin</option>
+                                <option value="Litecoin">Litecoin</option>
+                                <option value="...">...</option>
+                            </select> 
+                        </div>
+                        <div className="form-group">
+                            <label>Exchange</label>
+                            <select>
+                                <option value="Bisq">Bisq</option>
+                                <option value="Binance">Binance</option>
+                                <option value="...">...</option>
+                            </select> 
+                        </div>
+                    </div>
+                    <div className="flex-center">
+                        <LineChart width={600} height={350} data={btc}>
+                            <Line type="monotone" dataKey="value" stroke="#f00" />
+                            <XAxis dataKey="date"/>
+                            <YAxis allowDecimals={false} type="number" domain={['dataMin - 200', 'dataMax + 200']} />
+                        </LineChart>
+                    </div>
+                    <div className="flex-center">
+                        <button className="btn-graph btn">1d</button>
+                        <button className="btn btn-graph">5d</button>
+                        <button className="btn btn-graph">1m</button>
+                        <button className="btn btn-graph">6m</button>
+                        <button className="btn btn-graph">1y</button>
+                        <button className="btn btn-graph">5y</button>
+                    </div>
+                    {/* This second chart should be for international exchange prices */}
+                    <div>
+                        <div className="form-group">
+                            <label>Currency</label>
+                            <select>
+                                <option value="Dollar">Dollar</option>
+                                <option value="Euro">Euro</option>
+                                <option value="...">...</option>
+                            </select> 
+                        </div>
+                    </div>
+                    <div className="flex-center">
+                        <LineChart width={300} height={350} data={btc}>
+                            <Line type="monotone" dataKey="value" stroke="#f00" />
+                            <XAxis dataKey="date"/>
+                            <YAxis allowDecimals={false} type="number" domain={['dataMin - 200', 'dataMax + 200']} />
+                        </LineChart>
+                        <div>
+                            <button className="btn btn-graph">1d</button>
+                            <button className="btn btn-graph">5d</button>
+                            <button className="btn btn-graph">1m</button>
+                            <button className="btn btn-graph">6m</button>
+                            <button className="btn btn-graph">1y</button>
+                            <button className="btn btn-graph">5y</button>
+                        </div>
+                    </div>
+
+                    <h2>Recommended</h2>
                 </div>
             </div>
         )
